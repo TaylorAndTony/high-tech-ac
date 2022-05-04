b3ddeff6a94c5f7803cda795aa4cb8760d3911dd
TaylorAndTony <doyijian@163.com>
2022-05-04 21:37:45 +0800

    add maxwell and JS controled animation

--- change.js
+++ change.js
@@ -1,17 +1,35 @@
+function random(a, b) {
+    return Math.floor(Math.random() * (b - a + 1) + a);
+}
+
 async function sleep(mili) {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
         setTimeout(resolve, mili);
     });
 }
 
+function hideAll() {
+    for (let i of document.querySelectorAll('.swap-area')) {
+        i.style.display = 'none';
+    }
+}
+
+/**
+ * hide an element with animation
+ * @param {string} id The id of the element
+ */
 async function hide(id) {
     let a = document.getElementById(id);
     a.style.opacity = '0';
     await sleep(500);
     a.style.display = 'none';
-    
+
 }
 
+/**
+ * show an element with animation
+ * @param {string} id The id of the element
+ */
 async function show(id) {
     let a = document.getElementById(id);
     a.style.opacity = '1';
@@ -19,7 +37,8 @@ async function show(id) {
     a.style.display = 'block';
 }
 
-async function changeTemp(temp, color="") {
+// change the temp with animation effects
+async function changeTemp(temp, color = "") {
     let num = document.getElementById('temp-num');
     num.style.fontSize = '0px';
     num.style.opacity = '0';
@@ -33,26 +52,55 @@ async function changeTemp(temp, color="") {
 }
 
 function ac() {
-    hide('fan');
-    hide('machine');
+    hideAll();
     show('ac');
-    changeTemp(28,'#c99a3c');
+    changeTemp(28, '#c99a3c');
 }
 
-function fan(){
-    hide('ac');
-    hide('machine');
+function fan() {
+    hideAll();
     show('fan');
     changeTemp(35, '#ee4c54');
 }
 
 function machine() {
-    hide('ac');
-    hide('fan');
+    hideAll();
     show('machine');
     changeTemp(18, '#5768c9')
 }
 
-hide('ac');
-hide('machine');
-hide('fan');
+function maxwell() {
+    hideAll();
+    show('maxwell');
+    changeTemp(-273.15, '#da00e6');
+}
+
+// main func here
+window.onload = () => {
+    // first hide all machines
+    hideAll();
+    // random move machine
+    setInterval(function () {
+        let machine = document.getElementById('machine');
+        if (machine.style.display == 'none') { return; }
+        machine.style.transform = "translate("
+            + random(-20, 20)
+            + "px, "
+            + random(-20, 20)
+            + "px) rotate("
+            + random(-10, 10) + "deg)";
+    }, 100)
+    // random move maxwell
+    setInterval(function () {
+        let maxw = document.getElementById('maxwell');
+        if (maxw.style.display == 'none') { return; }
+        maxw.style.transform = "translate("
+            + random(-200, 200)
+            + "px, "
+            + random(-200, 200)
+            + "px) rotate("
+            + random(-100, 100) + "deg) scale("
+            + Math.random() * 1.5 + 1 + ")";
+    }, 1000)
+
+}
